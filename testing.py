ce009c0cb25ba01b3072e19f6352c379a99430df
KHITER Med Achraf <khiter.med7@gmail.com>
2024-08-10 19:39:51 +0200

    add regex to match emails ends with @gmail.com, return the users in alphabetic order

--- testing.py
+++ testing.py
@@ -1,3 +1,5 @@
+import re
+
 class TestDataEmptyArray:
     def get_array(self):
         return []
@@ -17,3 +19,22 @@ class TestDataExactlyTwoDifferentMinimums:
 
     def get_expected_result(self):
         return 3
+
+
+# DAY 28: work with regex to match emails end with @gmail.com, return the users in alphabitic order
+
+    N = int(input().strip())
+    # Regex pattern to match the required email format
+    pattern = r'^[a-z\.]+@gmail\.com$'
+    firstNames = []
+    for N_itr in range(N):
+        first_multiple_input = input().rstrip().split()
+
+        firstName = first_multiple_input[0]
+        emailID = first_multiple_input[1]
+
+        if re.match(pattern, emailID):
+            firstNames.append(firstName)
+
+    firstNames.sort()
+    print("\n".join(firstNames))
