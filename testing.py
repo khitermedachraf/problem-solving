1c324f1ab40531d63464e13705366c06ff23f941
KHITER Med Achraf <khiter.med7@gmail.com>
2024-08-10 21:53:53 +0200

    add Bitwise ADD operator manipulation

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 import re
 
+
 class TestDataEmptyArray:
     def get_array(self):
         return []
@@ -20,8 +21,7 @@ class TestDataExactlyTwoDifferentMinimums:
     def get_expected_result(self):
         return 3
 
-
-# DAY 28: work with regex to match emails end with @gmail.com, return the users in alphabitic order
+    # DAY 28: work with regex to match emails end with @gmail.com, return the users in alphabitic order
 
     N = int(input().strip())
     # Regex pattern to match the required email format
@@ -38,3 +38,23 @@ class TestDataExactlyTwoDifferentMinimums:
 
     firstNames.sort()
     print("\n".join(firstNames))
+
+
+# Betwise And: Task
+# Given set {1,2,3,...N} Find two integers, A and B (where A< B), from set  such that the value of A&B is the maximum possible and also less than a given integer K. In this case, & represents the bitwise AND operator.
+# Function Description
+# Complete the bitwiseAnd function in the editor below.
+# bitwiseAnd has the following paramter(s):
+# - int N: the maximum integer to consider
+# - int K: the limit of the result, inclusive
+
+
+def bitwiseAnd(n, k):
+    # Write your code here
+    maximum_value = 0
+    for i in range(1, n+1):
+        for j in range(i + 1, n+1):
+            if k > i & j > maximum_value:
+                maximum_value = i & j
+
+    return maximum_value
